93a79afd051d33029bf5bd12365ccef06e867027
Zavizenov <g_zavizenov@ats-hr420-dm15.fc.ca>
2019-10-03 13:40:21 -0400

    Finished js configuration

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
-const http = require('http');
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+const sql = require('./utils/sql');
 
-const hostname = '127.0.0.1';
-const port = process.enb.PORT || 3000;
+const port = process.env.PORT || 5050;
+const app = express();
+app.use(express.static('public'));
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname + '/views'));
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.get('/', (req, res) => {
+  res.render('home', { homemessage: "hey there", bio: "some generic bio info"});
+})
+
+app.get('/users', (req, res) => {
+  //get user data here
+
+  //try a database connection
+  // if a connection fails, error will be loged into a console
+  sql.getConnection((err, connection) => {
+    if (err) {
+      return console.log(err.message);
+    }
+
+    let query = "SELECT * FROM tbl_card";
+
+    sql.query(query, (err, rows) => {
+      connection.release();
+
+      if (err) {
+        return console.log(err.message);
+      }
+
+      console.log(rows);
+
+      res.render('user',rows[0]);
+    })
+  })
+})
+
+app.listen(port,() => {
+  console.log(`app is running on port ${port}`)
+})
---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    uname:"root",
+    upass: "root", // b;ank for windows
+    port: 8889, //3306 for windows
+    host: 'localhost',
+    db: 'biscard'
+}
+
+module.exports = config; 
